397a37a8395b86656d43a47c01f1dadc155bc72a
Luis Alho <lmalho@gmail.com>
2025-02-15 11:19:31 +0000

    update keymaps and lsp

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -3,15 +3,15 @@
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set('n', '<leader>w', ':w<CR>', {
-  desc = 'Save the current buffer',
+vim.keymap.set("n", "<leader>w", ":w<CR>", {
+	desc = "Save the current buffer",
 })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {
-  desc = 'Open diagnostic [Q]uickfix list',
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {
+	desc = "Open diagnostic [Q]uickfix list",
 })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -20,8 +20,8 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', {
-  desc = 'Exit terminal mode',
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", {
+	desc = "Exit terminal mode",
 })
 
 -- TIP: Disable arrow keys in normal mode
@@ -34,69 +34,69 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', {
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', {
-  desc = 'Move focus to the left window',
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", {
+	desc = "Move focus to the left window",
 })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', {
-  desc = 'Move focus to the right window',
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", {
+	desc = "Move focus to the right window",
 })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', {
-  desc = 'Move focus to the lower window',
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", {
+	desc = "Move focus to the lower window",
 })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', {
-  desc = 'Move focus to the upper window',
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", {
+	desc = "Move focus to the upper window",
 })
 
 -- Center when jumping
-vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', {
-  noremap = true,
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", {
+	noremap = true,
 })
-vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', {
-  noremap = true,
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", {
+	noremap = true,
 })
-vim.api.nvim_set_keymap('n', 'n', 'nzz', {
-  noremap = true,
+vim.api.nvim_set_keymap("n", "n", "nzz", {
+	noremap = true,
 })
-vim.api.nvim_set_keymap('n', 'N', 'Nzz', {
-  noremap = true,
+vim.api.nvim_set_keymap("n", "N", "Nzz", {
+	noremap = true,
 })
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- Move current line down
-vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv") -- Move current line up
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move current line down
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv") -- Move current line up
 
-vim.keymap.set('n', '<leader>v', ':vsplit<CR>', {
-  desc = 'Split vertically',
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", {
+	desc = "Split vertically",
 })
 -- Close buffer
-vim.api.nvim_set_keymap('n', '<leader>b', ':bdelete<CR>', {
-  noremap = true,
-  desc = 'Close the current buffer',
+vim.api.nvim_set_keymap("n", "<leader>b", ":bdelete!<CR>", {
+	noremap = true,
+	desc = "Close the current buffer",
 })
 
 -- Bufferline
-vim.api.nvim_set_keymap('n', 'gb', '<cmd>BufferLinePick<CR>', {
-  noremap = true,
-  desc = 'Bufferline Pick',
+vim.api.nvim_set_keymap("n", "gb", "<cmd>BufferLinePick<CR>", {
+	noremap = true,
+	desc = "Bufferline Pick",
 })
 
-vim.api.nvim_set_keymap('n', '<Tab>', '<cmd>BufferLineCycleNext<CR>', {
-  noremap = true,
-  desc = 'Bufferline Next',
+vim.api.nvim_set_keymap("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", {
+	noremap = true,
+	desc = "Bufferline Next",
 })
 
-vim.api.nvim_set_keymap('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', {
-  noremap = true,
-  desc = 'Bufferline Pick',
+vim.api.nvim_set_keymap("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", {
+	noremap = true,
+	desc = "Bufferline Pick",
 })
 
 -- Copilot
-vim.api.nvim_set_keymap('n', '<leader>ac', ':Copilot enable<CR>', {
-  noremap = true,
-  desc = 'Copilot Enable',
+vim.api.nvim_set_keymap("n", "<leader>ac", ":Copilot enable<CR>", {
+	noremap = true,
+	desc = "Copilot Enable",
 })
-vim.api.nvim_set_keymap('n', '<leader>aC', ':Copilot disable<CR>', {
-  noremap = true,
-  desc = 'Copilot Disable',
+vim.api.nvim_set_keymap("n", "<leader>aC", ":Copilot disable<CR>", {
+	noremap = true,
+	desc = "Copilot Disable",
 })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -104,14 +104,14 @@ vim.api.nvim_set_keymap('n', '<leader>aC', ':Copilot disable<CR>', {
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {
-    clear = true,
-  }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {
+		clear = true,
+	}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- vim: ts=2 sts=2 sw=2 et

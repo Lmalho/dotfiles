cec2f500edbd2923fa987f4226e99d38d6e71251
Luis Alho <luis.alho@paydock.com>
2025-12-30 21:36:49 +0000

    snacks picker

--- private_dot_config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -77,27 +77,27 @@ return {
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gd", Snacks.picker.lsp_definitions, "[G]oto [D]efinition")
 
 					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("gr", Snacks.picker.lsp_references, "[G]oto [R]eferences")
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("gI", Snacks.picker.lsp_implementations, "[G]oto [I]mplementation")
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("<leader>D", Snacks.picker.lsp_type_definitions, "Type [D]efinition")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>ld", require("telescope.builtin").lsp_document_symbols, "[D]ocument Symbols")
+					map("<leader>ld", Snacks.picker.lsp_symbols, "[D]ocument Symbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
-					map("<leader>lw", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace Symbols")
+					map("<leader>lw", Snacks.picker.lsp_workspace_symbols, "[W]orkspace Symbols")
 
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -12,7 +12,7 @@ return {
 		explorer = { enabled = false },
 		indent = { enabled = true },
 		input = { enabled = true },
-		picker = { enabled = false },
+		picker = { enabled = true },
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
@@ -20,4 +20,32 @@ return {
 		statuscolumn = { enabled = false },
 		words = { enabled = true },
 	},
+    keys = {
+        -- Top Pickers & Explorer
+        { "<leader><leader>", function() Snacks.picker.buffers() end, desc = "Buffers" },
+        { "<leader>/", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+        { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+        { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+        -- find
+        { "<leader>sf", function() Snacks.picker.files() end, desc = "Find Files" },
+        { "<leader>si", function() Snacks.picker.files({ hidden = true }) end, desc = "Find Hidden Files" },
+        { "<leader>s.", function() Snacks.picker.recent() end, desc = "Recent" },
+        -- git
+        { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+        { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+        -- Grep
+        { "<leader>sg", function() Snacks.picker.grep({ hidden = true }) end, desc = "Grep" },
+        { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word" },
+        { "<leader>s/", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+        -- search
+        { "<leader>ss", function() Snacks.picker.pickers() end, desc = "Search Pickers" },
+        { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+        { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+        { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+        { "<leader>sr", function() Snacks.picker.resume() end, desc = "Resume" },
+        -- Custom
+        { "<leader>sn", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+        { "<leader>so", function() Snacks.picker.files({ cwd = vim.fn.expand("~/obsidian/fnz") }) end, desc = "Find Obsidian File" },
+        { "<leader>sv", function() Snacks.picker.files() end, desc = "Split Vertical (Use Ctrl-v)" },
+    },
 }
